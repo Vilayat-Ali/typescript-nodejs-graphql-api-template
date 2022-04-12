665f850f4bb0553c44d55fc97b337ac3c5cc9159
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2022-04-12 13:28:29 +0530

    MongoDB schema models added

---
+++ src/models/member.ts
@@ -0,0 +1,17 @@
+import {model, Schema} from "mongoose";
+
+interface memberSchemaType{
+    name: string,
+    email: string,
+    password: string
+}
+
+const memberSchema = new Schema<memberSchemaType>({
+    name: { type: String, required: true},
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true}
+});
+
+const memberModel = model<memberSchemaType>('member', memberSchema);
+
+export default memberModel;
